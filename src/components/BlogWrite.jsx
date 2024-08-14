196c4aa17ace37d9e6ea8c9793979fce70eab3c7
usung325 <usung325@gmail.com>
2024-08-14 22:55:51 +0900

    Working BlogWrite component using useReducer

--- src/components/BlogWrite.jsx
+++ src/components/BlogWrite.jsx
@@ -1,42 +1,35 @@
 import React, { useState, useRef, useReducer } from 'react'
+import Card from './Card'
 
 export default function BlogWrite() {
 
-    const [blogList, setBlogList] = useState([])
+    // const [blogList, setBlogList] = useState([])
     const [blogTitle, setBlogTitle] = useState('')
-    const [blogContent, setBlogContent] = useState('{}')
+    const [blogContent, setBlogContent] = useState('')
+    const [im, setIm] = useState('')
+    const [audience, setAudience] = useState('')
 
     const submitBlog2List = (e) => {
         e.preventDefault()
-        setBlogList([
-            ...blogList, {
-                title: blogTitle,
-                content: blogContent
-            }
-        ])
-    }
-
-    const blogState = {
-        image: false,
-        textSize: 'sm'
+        dispatch({ style: 'ADD_BLOG', payload: { title: blogTitle, textContent: blogContent, im: im, audience: audience } })
+        setBlogTitle('')
+        setBlogContent('')
     }
 
     const reducer = (state, object) => {
-        switch (object.value) {
-            case "children":
-                return {
-                    image: true,
-                    textSize: '2xl'
+        switch (object.style) {
+            case "ADD_BLOG":
+
+                if (object.payload.audience === 'children') {
+                    return [...state, { title: object.payload.title, textContent: object.payload.textContent, imState: true, im: object.payload.im, textSize: '2xl' }]
                 }
-            case "adult":
-                return {
-                    image: false,
-                    textSize: 'sm'
+
+                else if (object.payload.audience === 'adult') {
+                    return [...state, { title: object.payload.title, textContent: object.payload.textContent, imState: false, im: null, textSize: 'sm' }]
                 }
-            case "both":
-                return {
-                    image: true,
-                    textSize: 'xl'
+
+                else {
+                    return [...state, { title: object.payload.title, textContent: object.payload.textContent, imState: true, im: object.payload.im, textSize: 'xl' }]
                 }
 
             default:
@@ -44,38 +37,67 @@ export default function BlogWrite() {
         }
     }
 
-    const [state, dispatch] = useReducer(reducer, blogList)
+    const [state, dispatch] = useReducer(reducer, [])
 
     return (
         <>
-            <form className="space-y-5 w-[30em] p-5" onSubmit={(e) => submitBlog2List(e)}>
-
-                <div>
-                    <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title: </label>
-                    <input type="text" id="base-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-                </div>
-
-
-                <div>
-                    <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Blog Content:</label>
-                    <textarea id="message" rows="15" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..."></textarea>
-                </div>
-                <div className="flex flex-row justify-between">
-                    <div>
-
-                        <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select your audience</label>
-                        <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                            <option value="">-- please select --</option>
-                            <option value="children">Children</option>
-                            <option value="adult">Adult</option>
-                            <option value="both">Both</option>
-                        </select>
+            <div className="flex flex-col items-center p-10">
+                <div className="flex overflow-auto max-w-full">
+                    <div className="flex flex-row space-x-10">
+                        {state.map((blog) => (
+                            <Card title={blog.title} textContent={blog.textContent} link={blog.im} />
+                        ))}
                     </div>
-
-
-                    <button type="submit">Submit</button>
                 </div>
-            </form >
+                <div className="">
+                    <form className="space-y-5 w-[30em] p-5 " onSubmit={(e) => submitBlog2List(e)}>
+
+                        <div>
+                            <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title: </label>
+                            <input type="text" id="base-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value={blogTitle} onChange={(e) => setBlogTitle(e.target.value)} />
+                        </div>
+
+                        <div>
+                            <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Blog Content:</label>
+                            <textarea id="message" rows="15" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..." value={blogContent} onChange={(e) => setBlogContent(e.target.value)}></textarea>
+                        </div>
+
+                        <div className="flex flex-row justify-between">
+
+                            <div>
+                                <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select your audience</label>
+                                <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={(e) => setAudience(e.target.value)}>
+                                    <option value="">-- please select --</option>
+                                    <option value="children">Children</option>
+                                    <option value="adult">Adult</option>
+                                    <option value="both">Both</option>
+                                </select>
+                            </div>
+
+                            <div>
+                                <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select your image</label>
+                                <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={(e) => setIm(e.target.value)}>
+                                    <option value="">-- please select --</option>
+                                    <option value="cat.jpeg">cat1</option>
+                                    <option value="cat2.jpeg">cat2</option>
+                                    <option value="cat3.jpeg">cat3</option>
+                                    <option value="cat4.jpeg">cat4</option>
+                                    <option value="cat5.jpeg">cat5</option>
+                                    <option value="cat6.jpeg">cat6</option>
+                                    <option value="cat7.jpeg">cat7</option>
+                                    <option value="cat8.jpeg">cat8</option>
+                                    <option value="cat9.jpeg">cat9</option>
+
+                                </select>
+                            </div>
+
+
+                            <button type="submit">Submit</button>
+                        </div>
+                    </form >
+                </div>
+            </div>
+
         </>
     )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,10 +4,10 @@ export default function Card({ title, textContent, link, textSecondary = '' }) {
 
 
     return (
-        <div className="bg-black max-w-80 min-h-[35em] rounded-lg flex flex-col ">
+        <div className="bg-black max-w-80 min-h-[35em] rounded-lg flex flex-col min-w-[20em]">
 
             <div>
-                <img src={`/images/${link}`} className="object-cover overflow-hidden" />
+                {link !== null ? <img src={`/images/${link}`} className="object-cover overflow-hidden" /> : null}
             </div>
 
             <div className="flex flex-grow flex-col mt-4 mb-14 text-white pt-4 px-2">
